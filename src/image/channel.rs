4f386bd51594eb18f78af4b34bfd28a05b36b9eb
Postrex <post-rex@pm.me>
2020-11-19 09:07:30 +0100

    Properly devided Channel into read and write section I assme this code is memory safe, I hope it is... If there is a better way to implement this someone send it to me in like 5y+ from now when im rich and famous or something, hehe

--- src/image/channel.rs
+++ src/image/channel.rs
@@ -1,26 +1,36 @@
-use std::ops::Deref;
-use std::{alloc, ptr, sync};
+use std::sync::atomic;
+use std::{alloc, ptr};
 
 pub struct Channel {
     inner: *mut f32,
     layout: alloc::Layout,
     width: usize,
     height: usize,
-    ref_counter: sync::Arc<()>,
+    ref_counter: ptr::NonNull<atomic::AtomicIsize>,
+}
+
+pub struct WriteGuard<'a> {
+    channel: &'a mut Channel,
+    ref_counter: ptr::NonNull<atomic::AtomicIsize>,
+}
+
+pub struct ReadGuard<'a> {
+    channel: &'a Channel,
+    ref_counter: ptr::NonNull<atomic::AtomicIsize>,
 }
 
 pub struct WriteIter<'a> {
     channel: &'a mut Channel,
     step: usize,
-    ref_counter: sync::Arc<()>,
+    ref_counter: ptr::NonNull<atomic::AtomicIsize>,
 }
 
-pub struct WriteGuard {
+pub struct WriteIterGuard {
     inner: *mut [f32],
     offset: usize,
     width: usize,
     height: usize,
-    ref_counter: sync::Arc<()>,
+    ref_counter: ptr::NonNull<atomic::AtomicIsize>,
 }
 
 impl Channel {
@@ -31,71 +41,39 @@ impl Channel {
             width,
             height,
             layout,
-            ref_counter: sync::Arc::new(()),
+            ref_counter: Box::leak(Box::new(atomic::AtomicIsize::new(0))).into(),
         }
     }
 
-    pub fn read_raw(&self) -> &[f32] {
-        unsafe { &*ptr::slice_from_raw_parts(self.inner as *const f32, self.width * self.height) }
-    }
-
-    pub fn write_raw(&mut self) -> &mut [f32] {
-        unsafe { &mut *ptr::slice_from_raw_parts_mut(self.inner, self.width * self.height) }
-    }
-
-    pub fn chunked_iter_mut(&mut self) -> WriteIter {
-        let rc = self.ref_counter.clone();
-        WriteIter {
+    pub fn lock_write(&mut self) -> WriteGuard {
+        // FIXME: This is a horrible solution, please fix this
+        while self.locked() != 0 {
+            std::thread::yield_now();
+        }
+        unsafe { self.ref_counter.as_ref() }.store(-1, atomic::Ordering::Release);
+        let rc = self.ref_counter;
+        WriteGuard {
             channel: self,
-            step: 0,
+            ref_counter: rc,
+        }
+    }
+
+    pub fn lock_read(&self) -> ReadGuard {
+        // FIXME: This is a horrible solution, please fix this
+        while self.locked() < 0 {
+            std::thread::yield_now();
+        }
+        unsafe { self.ref_counter.as_ref() }.fetch_sub(1, atomic::Ordering::AcqRel);
+        let rc = self.ref_counter;
+        ReadGuard {
+            channel: self,
             ref_counter: rc,
         }
     }
 
     #[inline]
-    pub fn locked(&self) -> bool {
-        sync::Arc::strong_count(&self.ref_counter) != 1
-    }
-}
-
-impl Clone for Channel {
-    fn clone(&self) -> Self {
-        let layout = self.layout;
-        let inner = unsafe { alloc::alloc_zeroed(layout) as *mut f32 };
-        unsafe {
-            ptr::copy_nonoverlapping(
-                self.inner as *const u8,
-                inner as *mut u8,
-                self.layout.size(),
-            );
-        }
-        Self {
-            inner,
-            width: self.width,
-            height: self.height,
-            layout,
-            ref_counter: sync::Arc::new(()),
-        }
-    }
-
-    fn clone_from(&mut self, source: &Self) {
-        if self.width == source.width && self.height == source.height {
-            unsafe {
-                ptr::copy_nonoverlapping(
-                    source.inner as *const u8,
-                    self.inner as *mut u8,
-                    self.layout.size(),
-                );
-            }
-        } else {
-            unsafe {
-                alloc::dealloc(self.inner as *mut u8, self.layout);
-                self.inner = std::alloc::alloc_zeroed(source.layout) as *mut f32;
-            }
-            self.layout = source.layout;
-            self.width = source.width;
-            self.height = source.height;
-        }
+    pub fn locked(&self) -> isize {
+        unsafe { self.ref_counter.as_ref() }.load(atomic::Ordering::Acquire)
     }
 }
 
@@ -105,8 +83,85 @@ impl Drop for Channel {
     }
 }
 
+impl<'a> WriteGuard<'a> {
+    pub fn write_raw(&mut self) -> &mut [f32] {
+        unsafe {
+            &mut *ptr::slice_from_raw_parts_mut(
+                self.channel.inner,
+                self.channel.width * self.channel.height,
+            )
+        }
+    }
+
+    pub fn chunked_iter_mut(&mut self) -> WriteIter {
+        let rc = self.ref_counter;
+        WriteIter {
+            channel: self.channel,
+            step: 0,
+            ref_counter: rc,
+        }
+    }
+
+    fn clone_from(&mut self, source: &ReadGuard) {
+        if self.channel.width == source.channel.width
+            && self.channel.height == source.channel.height
+        {
+            unsafe {
+                ptr::copy_nonoverlapping(
+                    source.channel.inner as *const u8,
+                    self.channel.inner as *mut u8,
+                    self.channel.layout.size(),
+                );
+            }
+        } else {
+            unsafe {
+                alloc::dealloc(self.channel.inner as *mut u8, self.channel.layout);
+                self.channel.inner = std::alloc::alloc_zeroed(source.channel.layout) as *mut f32;
+            }
+            self.channel.layout = source.channel.layout;
+            self.channel.width = source.channel.width;
+            self.channel.height = source.channel.height;
+        }
+    }
+}
+
+impl<'a> Drop for WriteGuard<'a> {
+    fn drop(&mut self) {
+        unsafe { self.ref_counter.as_ref() }.store(0, atomic::Ordering::Release);
+    }
+}
+
+impl<'a> ReadGuard<'a> {
+    pub fn read_raw(&self) -> &[f32] {
+        unsafe {
+            &*ptr::slice_from_raw_parts(
+                self.channel.inner as *const f32,
+                self.channel.width * self.channel.height,
+            )
+        }
+    }
+
+    fn clone(&self) -> Channel {
+        let channel = Channel::new(self.channel.width, self.channel.height);
+        unsafe {
+            ptr::copy_nonoverlapping(
+                self.channel.inner as *const u8,
+                channel.inner as *mut u8,
+                self.channel.layout.size(),
+            );
+        }
+        channel
+    }
+}
+
+impl<'a> Drop for ReadGuard<'a> {
+    fn drop(&mut self) {
+        unsafe { self.ref_counter.as_ref() }.fetch_add(1, atomic::Ordering::AcqRel);
+    }
+}
+
 impl<'a> Iterator for WriteIter<'a> {
-    type Item = WriteGuard;
+    type Item = WriteIterGuard;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.step >= self.channel.height {
@@ -114,7 +169,7 @@ impl<'a> Iterator for WriteIter<'a> {
         } else {
             let offset = self.channel.height * self.step;
             self.step += 1;
-            Some(WriteGuard {
+            Some(WriteIterGuard {
                 inner: ptr::slice_from_raw_parts_mut(
                     unsafe { self.channel.inner.add(offset) },
                     self.channel.width,
@@ -139,7 +194,10 @@ impl<'a> Iterator for WriteIter<'a> {
     }
 }
 
-impl WriteGuard {
+unsafe impl Send for WriteIterGuard {}
+
+impl WriteIterGuard {
+    // Fixme: This is horrible
     pub fn offsets(&self) -> ((u32, u32), (u32, u32)) {
         (
             (
--- src/image/tests.rs
+++ src/image/tests.rs
@@ -1,23 +1,59 @@
 #[test]
 #[allow(clippy::float_cmp)]
-fn channel_test() {
+fn channel_test_st() {
     use crate::image::Channel;
 
     let mut c = Channel::new(2, 2);
+    assert_eq!(c.locked(), 0);
     assert_eq!(
-        c.read_raw(),
+        c.lock_read().read_raw(),
         &[0.0, 0.0, 0.0, 0.0],
         "Channel should be zeroed"
     );
-    let writeable = c.write_raw();
-    writeable[1] = 0.25;
-    writeable[2] = 0.5;
-    writeable[3] = 0.75;
+    assert_eq!(c.locked(), 0);
+    {
+        let mut lock = c.lock_write();
+        let writeable = lock.write_raw();
+        writeable[1] = 0.25;
+        writeable[2] = 0.5;
+        writeable[3] = 0.75;
+    }
+    assert_eq!(c.locked(), 0);
     assert_eq!(
-        c.read_raw(),
+        c.lock_read().read_raw(),
         &[0.0, 0.25, 0.5, 0.75],
         "Channel should contain values, but doesn't"
     );
+    assert_eq!(c.locked(), 0);
 
     // let mut iter = c.chunked_iter_mut(); // TODO: add test case here
+}
+
+#[test]
+#[allow(clippy::float_cmp)]
+fn channel_test_mt() {
+    use crate::image::Channel;
+
+    let mut c = Channel::new(2, 2);
+
+    let bar = std::sync::Arc::new(std::sync::Barrier::new(3));
+
+    {
+        let mut wl = c.lock_write();
+        wl.chunked_iter_mut().for_each(|mut block| {
+            let bc = bar.clone();
+            std::thread::spawn(move || {
+                let off = block.offsets().0;
+                block.get_mut()[0] = off.0 as f32 + off.1 as f32 * 2.0;
+                block.get_mut()[1] = off.0 as f32 + off.1 as f32 * 2.0 + 1.0;
+                bc.wait();
+            });
+        })
+    }
+    bar.wait();
+    assert_eq!(
+        c.lock_read().read_raw(),
+        &[0.0, 1.0, 2.0, 3.0],
+        "Channel should contain values, but doesn't"
+    );
 }
