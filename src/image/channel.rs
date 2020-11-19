d5f93f104b99bdaf1dd45f29957ca45ca749ef36
post-rex <post-rex@pm.me>
2020-11-19 01:46:53 +0100

    Reminder to hide clone etc behind a read guard and writ stuff behind a write guard to prevent data races

--- src/image/channel.rs
+++ src/image/channel.rs
@@ -1,48 +1,60 @@
-use std::{alloc, ptr};
+use std::ops::Deref;
+use std::{alloc, ptr, sync};
 
 pub struct Channel {
     inner: *mut f32,
     layout: alloc::Layout,
-    width: u32,
-    height: u32,
+    width: usize,
+    height: usize,
+    ref_counter: sync::Arc<()>,
 }
 
 pub struct WriteIter<'a> {
     channel: &'a mut Channel,
-    step: u32,
+    step: usize,
+    ref_counter: sync::Arc<()>,
+}
+
+pub struct WriteGuard {
+    inner: *mut [f32],
+    offset: usize,
+    width: usize,
+    height: usize,
+    ref_counter: sync::Arc<()>,
 }
 
 impl Channel {
-    pub fn new(width: u32, height: u32) -> Self {
-        let layout = alloc::Layout::array::<f32>((width * height) as usize).unwrap();
+    pub fn new(width: usize, height: usize) -> Self {
+        let layout = alloc::Layout::array::<f32>(width * height).unwrap();
         Self {
             inner: unsafe { alloc::alloc_zeroed(layout) as *mut f32 },
             width,
             height,
             layout,
+            ref_counter: sync::Arc::new(()),
         }
     }
 
     pub fn read_raw(&self) -> &[f32] {
-        unsafe {
-            &*ptr::slice_from_raw_parts(
-                self.inner as *const f32,
-                (self.width * self.height) as usize,
-            )
-        }
+        unsafe { &*ptr::slice_from_raw_parts(self.inner as *const f32, self.width * self.height) }
     }
 
     pub fn write_raw(&mut self) -> &mut [f32] {
-        unsafe {
-            &mut *ptr::slice_from_raw_parts_mut(self.inner, (self.width * self.height) as usize)
-        }
+        unsafe { &mut *ptr::slice_from_raw_parts_mut(self.inner, self.width * self.height) }
     }
 
     pub fn chunked_iter_mut(&mut self) -> WriteIter {
+        let rc = self.ref_counter.clone();
         WriteIter {
             channel: self,
             step: 0,
+            ref_counter: rc,
         }
+    }
+
+    #[inline]
+    pub fn locked(&self) -> bool {
+        sync::Arc::strong_count(&self.ref_counter) != 1
     }
 }
 
@@ -62,6 +74,7 @@ impl Clone for Channel {
             width: self.width,
             height: self.height,
             layout,
+            ref_counter: sync::Arc::new(()),
         }
     }
 
@@ -93,35 +106,58 @@ impl Drop for Channel {
 }
 
 impl<'a> Iterator for WriteIter<'a> {
-    type Item = *mut [f32];
+    type Item = WriteGuard;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.step >= self.channel.height {
             None
         } else {
+            let offset = self.channel.height * self.step;
             self.step += 1;
-            Some(ptr::slice_from_raw_parts_mut(
-                unsafe {
-                    self.channel
-                        .inner
-                        .add((self.channel.height * (self.step - 1)) as usize)
-                },
-                self.channel.width as usize,
-            ))
+            Some(WriteGuard {
+                inner: ptr::slice_from_raw_parts_mut(
+                    unsafe { self.channel.inner.add(offset) },
+                    self.channel.width,
+                ),
+                offset,
+                width: self.channel.width,
+                height: self.channel.height,
+                ref_counter: self.ref_counter.clone(),
+            })
         }
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (
-            self.channel.height as usize,
-            Some(self.channel.height as usize),
-        )
+        (self.channel.height, Some(self.channel.height))
     }
 
     fn count(self) -> usize
     where
         Self: Sized,
     {
-        self.channel.height as usize
+        self.channel.height
+    }
+}
+
+impl WriteGuard {
+    pub fn offsets(&self) -> ((u32, u32), (u32, u32)) {
+        (
+            (
+                (self.offset % self.width) as u32,
+                (self.offset / self.width) as u32,
+            ),
+            (
+                (self.offset % self.width + self.offset) as u32,
+                (self.offset / self.width) as u32,
+            ),
+        )
+    }
+
+    pub fn get(&self) -> &[f32] {
+        unsafe { &*self.inner }
+    }
+
+    pub fn get_mut(&self) -> &mut [f32] {
+        unsafe { &mut *self.inner }
     }
 }
