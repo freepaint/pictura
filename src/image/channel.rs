ca743dd316f04a257a63cf0ba1fd033fc1ae71ed
Postrex <post-rex@pm.me>
2020-12-10 09:30:12 +0100

    Making data of RW/Lock accessable

--- src/image/channel.rs
+++ src/image/channel.rs
@@ -1,6 +1,6 @@
 use nalgebra::Vector2;
 use std::alloc::Layout;
-use std::sync::atomic;
+use std::ops::{Index, IndexMut};
 use std::{alloc, ptr};
 use tokio::sync::Semaphore;
 
@@ -10,21 +10,25 @@ const MAX_PERMITS: usize = usize::MAX >> 3;
 pub struct Channel<U: Sized = f32> {
     inner: *mut U,
     memory_layout: alloc::Layout,
-    size: Vector2<usize>,
+    size: Vector2<u32>,
     lock: tokio::sync::Semaphore,
 }
 
-pub struct WritePermit<'a> {
+pub struct ReadGuard<'a, U: Sized = f32> {
     lock: tokio::sync::SemaphorePermit<'a>,
+    channel: &'a Channel<U>,
 }
 
-pub struct ReadGuard<'a> {
+pub struct WritePermit<'a, U: Sized = f32> {
     lock: tokio::sync::SemaphorePermit<'a>,
+    channel: &'a Channel<U>,
 }
+
+pub struct WriteIter<'a, U: Sized = f32> {}
 
 impl<U: Sized> Channel<U> {
-    pub fn new(size: Vector2<usize>) -> Result<Self, crate::error::Error> {
-        let layout = Layout::array(size.x * size.y)?;
+    pub fn new(size: Vector2<u32>) -> Result<Self, crate::error::AllocError> {
+        let layout = Layout::array::<U>((size.x * size.y) as usize)?;
         let data = unsafe { std::alloc::alloc_zeroed(layout) };
         if data.is_null() {
             return Err(crate::error::AllocError::Alloc.into());
@@ -37,17 +41,101 @@ impl<U: Sized> Channel<U> {
         })
     }
 
-    pub async fn write(&self) -> WritePermit {
-        WritePermit {
-            lock: self.lock.acquire_many(MAX_PERMITS as u32).await,
+    fn within(&self, pt: &Vector2<u32>) -> bool {
+        0 <= pt.x && pt.x < self.size.x as u32 && 0 <= pt.y && pt.y < self.size.y as u32
+    }
+
+    unsafe fn offset(&self, pt: &Vector2<u32>) -> *mut U {
+        self.inner
+            .clone()
+            .add((pt.y * self.size.x as u32 + pt.x) as usize)
+    }
+
+    unsafe fn read_unchecked(&self, pt: &Vector2<u32>) -> &U {
+        self.offset(pt).as_ref().unwrap()
+    }
+
+    unsafe fn write_unchecked(&self, pt: &Vector2<u32>) -> &mut U {
+        self.offset(pt).as_mut().unwrap()
+    }
+
+    pub async fn read(&self) -> ReadGuard<'_, U> {
+        ReadGuard {
+            lock: self.lock.acquire().await,
+            channel: self,
         }
     }
 
-    pub async fn read(&self) -> ReadGuard {
-        ReadGuard {
-            lock: self.lock.acquire().await,
+    pub async fn write(&self) -> WritePermit<'_, U> {
+        WritePermit {
+            lock: self.lock.acquire_many(MAX_PERMITS as u32).await,
+            channel: self,
         }
     }
 }
 
-impl WritePermit {}
+impl<'a, U: Sized> ReadGuard<'a, U> {
+    pub fn size(&self) -> Vector2<u32> {
+        self.channel.size.map(|n| n as u32)
+    }
+}
+
+impl<'a, U: Sized> WritePermit<'a, U> {
+    pub fn size(&self) -> Vector2<u32> {
+        self.channel.size.map(|n| n as u32)
+    }
+
+    pub fn iter_mut(&mut self) -> WriteIter {}
+}
+
+impl Iterator for WriteIter {
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        todo!()
+    }
+
+    fn count(self) -> usize
+    where
+        Self: Sized,
+    {
+        self.size_hint().0
+    }
+}
+
+impl<'a, U: Sized> Index<&Vector2<u32>> for ReadGuard<'a, U> {
+    type Output = U;
+
+    fn index(&self, index: &Vector2<u32>) -> &Self::Output {
+        check_bounds(&index, &self.channel);
+        unsafe { self.channel.read_unchecked(index) }
+    }
+}
+
+impl<'a, U: Sized> Index<&Vector2<u32>> for WritePermit<'a, U> {
+    type Output = U;
+
+    fn index(&self, index: &Vector2<u32>) -> &Self::Output {
+        check_bounds(&index, &self.channel);
+        unsafe { self.channel.read_unchecked(index) }
+    }
+}
+
+impl<'a, U: Sized> IndexMut<&Vector2<u32>> for WritePermit<'a, U> {
+    fn index_mut(&mut self, index: &Vector2<u32>) -> &mut Self::Output {
+        check_bounds(&index, &self.channel);
+        unsafe { self.channel.write_unchecked(index) }
+    }
+}
+
+#[inline(always)]
+fn check_bounds<U: Sized>(pt: &Vector2<u32>, ch: &Channel<U>) {
+    if !ch.within(pt) {
+        panic!(
+            "Unable to access {:?} with bounds (0, 0) -> {:?}",
+            pt, &ch.size.x,
+        );
+    }
+}
